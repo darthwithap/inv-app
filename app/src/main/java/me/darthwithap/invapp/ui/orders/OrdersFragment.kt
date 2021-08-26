303bd9fdf8b7d5a86182d5c93fbd3f1d0787d422
Parth <parth.t@gtgt.in>
2021-08-26 11:08:47 +0530

    Push code
    Signed-off-by: Parth <parth.t@gtgt.in>

--- app/src/main/java/me/darthwithap/invapp/ui/orders/OrdersFragment.kt
+++ app/src/main/java/me/darthwithap/invapp/ui/orders/OrdersFragment.kt
@@ -2,7 +2,6 @@ package me.darthwithap.invapp.ui.orders
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,141 +18,138 @@ import me.darthwithap.invapp.ui.orders.adapter.GodownOrderAdapter
 import me.darthwithap.invapp.ui.viewmodel.GodownViewModel
 import me.darthwithap.invapp.ui.viewmodel.InvoiceViewModel
 
-private const val TAG = "TAG"
-
 class OrdersFragment : Fragment() {
 
-    private lateinit var godownViewModel: GodownViewModel
-    private lateinit var invoiceViewModel: InvoiceViewModel
-    private var _binding: FragmentOrdersBinding? = null
-    private val binding get() = _binding
+  private lateinit var godownViewModel: GodownViewModel
+  private lateinit var invoiceViewModel: InvoiceViewModel
+  private var _binding: FragmentOrdersBinding? = null
+  private val binding get() = _binding
 
-    private var godownIndex = 0
-    private var globalCheckCount = 0
+  private var godownIndex = 0
+  private var globalCheckCount = 0
 
-    private lateinit var godownChipAdapter: GodownChipAdapter
-    private lateinit var godownOrdersAdapter: GodownOrderAdapter
-    private val godowns: ArrayList<Godown> = arrayListOf()
-    private val invoices: ArrayList<Invoice> = arrayListOf()
-    private var currGodownId: String = ""
-    private val pendingOrdersDataMap: HashMap<String, ArrayList<String>> = hashMapOf()
+  private lateinit var godownChipAdapter: GodownChipAdapter
+  private lateinit var godownOrdersAdapter: GodownOrderAdapter
+  private val godowns: ArrayList<Godown> = arrayListOf()
+  private val invoices: ArrayList<Invoice> = arrayListOf()
+  private var currGodownId: String = ""
+  private val pendingOrdersDataMap: HashMap<String, ArrayList<String>> = hashMapOf()
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        godownViewModel =
-            ViewModelProvider(this).get(GodownViewModel::class.java)
-        invoiceViewModel =
-            ViewModelProvider(this).get(InvoiceViewModel::class.java)
+  @RequiresApi(Build.VERSION_CODES.N)
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    godownViewModel =
+      ViewModelProvider(this).get(GodownViewModel::class.java)
+    invoiceViewModel =
+      ViewModelProvider(this).get(InvoiceViewModel::class.java)
 
-        godownViewModel.getAllGodowns()
+    godownViewModel.getAllGodowns()
 
-        _binding = FragmentOrdersBinding.inflate(inflater, container, false)
+    _binding = FragmentOrdersBinding.inflate(inflater, container, false)
 
-        // Godown Chip RecyclerView Setup
-        _binding?.rvGodownChips?.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    // Godown Chip RecyclerView Setup
+    _binding?.rvGodownChips?.layoutManager =
+      LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
-        godownChipAdapter = GodownChipAdapter(godowns) { id, _ ->
-            godownViewModel.onCurrentGodownChanged(id)
+    godownChipAdapter = GodownChipAdapter(godowns) { id, _ ->
+      godownViewModel.onCurrentGodownChanged(id)
+    }
+
+    godownOrdersAdapter = GodownOrderAdapter(invoices) { _, isChecked, productId, invoiceId ->
+      pendingOrdersDataMap.getOrPut(invoiceId, { arrayListOf() })
+      if (isChecked) {
+        globalCheckCount++
+        pendingOrdersDataMap[invoiceId]?.add(productId)
+      } else {
+        globalCheckCount--
+        pendingOrdersDataMap[invoiceId]?.removeIf { it == productId }
+      }
+    }
+    _binding?.rvGodownOrders?.adapter = godownOrdersAdapter
+
+    _binding?.rvGodownChips?.adapter = godownChipAdapter
+
+    _binding?.btnDone?.setOnClickListener {
+      if (pendingOrdersDataMap.isNotEmpty()) {
+        pendingOrdersDataMap.forEach {
+          if (it.value.isNotEmpty()) {
+            invoiceViewModel.updatePendingOrdersStatusFor(it.key, it.value)
+          }
         }
-
-        godownOrdersAdapter = GodownOrderAdapter(invoices) { _, isChecked, productId, invoiceId ->
-            pendingOrdersDataMap.getOrPut(invoiceId, { arrayListOf() })
-            if (isChecked) {
-                globalCheckCount++
-                pendingOrdersDataMap[invoiceId]?.add(productId)
-            } else {
-                globalCheckCount--
-                pendingOrdersDataMap[invoiceId]?.removeIf { it == productId }
-            }
-            Log.d(TAG, "onCreateView: $pendingOrdersDataMap")
-        }
-        _binding?.rvGodownOrders?.adapter = godownOrdersAdapter
-
-        _binding?.rvGodownChips?.adapter = godownChipAdapter
-
-        _binding?.btnDone?.setOnClickListener {
-            if (pendingOrdersDataMap.isNotEmpty()) {
-                pendingOrdersDataMap.forEach {
-                    if (it.value.isNotEmpty()) {
-                        invoiceViewModel.updatePendingOrdersStatusFor(it.key, it.value)
-                    }
-                }
-            } else {
-                showError("No products set as delivered")
-            }
-        }
-
-        return binding?.root
+      } else {
+        showError("No products set as delivered")
+      }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    return binding?.root
+  }
 
-        godownViewModel.godownsResult.observe(viewLifecycleOwner, {
-            //loading.visibility = View.GONE
-            if (it.error != null) {
-                showError(it.error)
-            }
-            if (it.success != null) {
-                updateUi(it.success)
-            }
-        })
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        godownViewModel.currGodownId.observe(viewLifecycleOwner, {
-            if (currGodownId != it) godownChipAdapter.setCurrentGodown(it)
-            godownChipAdapter.notifyDataSetChanged()
-            invoiceViewModel.getPendingOrdersForGodown(it)
-            pendingOrdersDataMap.clear()
-        })
+    godownViewModel.godownsResult.observe(viewLifecycleOwner, {
+      //loading.visibility = View.GONE
+      if (it.error != null) {
+        showError(it.error)
+      }
+      if (it.success != null) {
+        updateUi(it.success)
+      }
+    })
 
-        invoiceViewModel.pendingOrdersResult.observe(viewLifecycleOwner, {
-            if (it.error != null) {
-                showError(it.error)
-            }
-            if (it.success != null) {
-                updateGodownOrders(it.success)
-            }
-        })
+    godownViewModel.currGodownId.observe(viewLifecycleOwner, {
+      if (currGodownId != it) godownChipAdapter.setCurrentGodown(it) //updateStory
+      godownChipAdapter.notifyDataSetChanged()
+      invoiceViewModel.getPendingOrdersForGodown(it) // loadData
+      pendingOrdersDataMap.clear()
+    })
 
-        invoiceViewModel.pendingOrdersUpdateStatusResult.observe(viewLifecycleOwner, {
-            if (it.error != null) {
-                showError(it.error)
-            }
-            if (it.success != null) {
-                showSuccess(it.success)
-            }
-        })
-    }
+    invoiceViewModel.pendingOrdersResult.observe(viewLifecycleOwner, {
+      if (it.error != null) {
+        showError(it.error)
+      }
+      if (it.success != null) {
+        updateGodownOrders(it.success)
+      }
+    })
 
-    private fun updateGodownOrders(list: List<Invoice>) {
-        _binding?.rvGodownOrders?.layoutManager = LinearLayoutManager(context)
-        invoices.clear()
-        invoices.addAll(list)
-        godownOrdersAdapter.notifyDataSetChanged()
-    }
+    invoiceViewModel.pendingOrdersUpdateStatusResult.observe(viewLifecycleOwner, {
+      if (it.error != null) {
+        showError(it.error)
+      }
+      if (it.success != null) {
+        showSuccess(it.success)
+      }
+    })
+  }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+  private fun updateGodownOrders(list: List<Invoice>) {
+    _binding?.rvGodownOrders?.layoutManager = LinearLayoutManager(context)
+    invoices.clear()
+    invoices.addAll(list)
+    godownOrdersAdapter.notifyDataSetChanged()
+  }
 
-    private fun updateUi(list: List<Godown>) {
-        godownViewModel.onCurrentGodownChanged(list[godownIndex].godownId)
-        godowns.clear()
-        godowns.addAll(list)
-        godownChipAdapter.notifyDataSetChanged()
-    }
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
 
-    private fun showError(error: String) {
-        context?.let { Toasty.error(it, error).show() }
-    }
+  private fun updateUi(list: List<Godown>) {
+    godownViewModel.onCurrentGodownChanged(list[godownIndex].godownId)
+    godowns.clear()
+    godowns.addAll(list)
+    godownChipAdapter.notifyDataSetChanged()
+  }
 
-    private fun showSuccess(success: String) {
-        context?.let { Toasty.success(it, success).show() }
-    }
+  private fun showError(error: String) {
+    context?.let { Toasty.error(it, error).show() }
+  }
+
+  private fun showSuccess(success: String) {
+    context?.let { Toasty.success(it, success).show() }
+  }
 }
--- app/src/main/java/me/darthwithap/invapp/ui/orders/adapter/GodownChipAdapter.kt
+++ app/src/main/java/me/darthwithap/invapp/ui/orders/adapter/GodownChipAdapter.kt
@@ -9,44 +9,44 @@ import me.darthwithap.invapp.data.domain.models.Godown
 import me.darthwithap.invapp.databinding.ListItemGodownChipBinding
 
 class GodownChipAdapter(
-    private val godowns: List<Godown>,
-    private val onClick: (id: String, name: String) -> Unit
+  private val godowns: List<Godown>,
+  private val onClick: (id: String, name: String) -> Unit
 ) :
-    RecyclerView.Adapter<GodownChipAdapter.ViewHolder>() {
+  RecyclerView.Adapter<GodownChipAdapter.ViewHolder>() {
 
-    private var currGodown: String = ""
+  private var currGodown: String = ""
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ListItemGodownChipBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
-        return ViewHolder(binding.root)
-    }
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    val binding = ListItemGodownChipBinding.inflate(
+      LayoutInflater.from(parent.context),
+      parent,
+      false
+    )
+    return ViewHolder(binding.root)
+  }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(godowns[position])
-    }
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.bind(godowns[position])
+  }
 
-    override fun getItemCount(): Int {
-        return godowns.size
-    }
+  override fun getItemCount(): Int {
+    return godowns.size
+  }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: Godown) {
-            ListItemGodownChipBinding.bind(itemView).apply {
-                with(item) {
-                    if (currGodown == godownId) root.setChipBackgroundColorResource(R.color.colorPrimary)
-                    else root.setChipBackgroundColorResource(R.color.colorPrimary20)
-                    tvGodownName.text = name
-                    root.setOnClickListener { onClick.invoke(godownId, name) }
-                }
-            }
+  inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(item: Godown) {
+      ListItemGodownChipBinding.bind(itemView).apply {
+        with(item) {
+          if (currGodown == godownId) root.setChipBackgroundColorResource(R.color.colorPrimary)
+          else root.setChipBackgroundColorResource(R.color.colorPrimary20)
+          tvGodownName.text = name
+          root.setOnClickListener { onClick.invoke(godownId, name) }
         }
+      }
     }
+  }
 
-    fun setCurrentGodown(godownId: String) {
-        currGodown = godownId
-    }
+  fun setCurrentGodown(godownId: String) {
+    currGodown = godownId
+  }
 }
